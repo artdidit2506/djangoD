f9b046c411ef93e041df8a442048990f8d04fffa
artdidit2506 <artdidit2506@gmail.com>
2022-05-27 23:25:50 +0700

    menmabhkan beberapa file di templates

--- base/views.py
+++ base/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    return render(request, 'home.html')
 
 def room(request):
-    return HttpResponse('<h1>Hello Rooms!</h1>')
+    return render(request, 'room.html')
