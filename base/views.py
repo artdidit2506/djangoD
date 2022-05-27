7c91542a8c8bf9fa731ca56396e76923d153563c
artdidit2506 <artdidit2506@gmail.com>
2022-05-27 23:46:58 +0700

    edit file

--- base/views.py
+++ base/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn Python'},
+    {'id': 2, 'name': 'Lets learn Django'},
+    {'id': 3, 'name': 'Lets learn Flask'},
+]
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 def room(request):
     return render(request, 'room.html')
